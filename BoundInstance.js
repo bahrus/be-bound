7d8667accc4c9abc26bdde3ccc245cc5c5b12e7e
Bruce Anderson <anderson.bruce.b@gmail.com>
2022-08-01 05:20:19 -0400

    Initial commit

--- BoundInstance.js
+++ BoundInstance.js
@@ -2,20 +2,29 @@ import { subscribe } from 'trans-render/lib/subscribe.js';
 export class BoundInstance {
     childProp;
     hostProp;
+    child;
     host;
     options;
     constructor(childProp, hostProp, child, host, options) {
         this.childProp = childProp;
         this.hostProp = hostProp;
+        this.child = child;
         this.host = host;
         this.options = options;
-        subscribe(child, childProp, this.updateHost);
+        if (child.localName === 'input' && childProp === 'value') {
+            child.addEventListener('input', this.updateHost);
+        }
+        else {
+            subscribe(child, childProp, this.updateHost);
+        }
         subscribe(host, hostProp, this.updateChild);
     }
     updateHost = () => {
         console.log('updateHost');
+        this.host[this.hostProp] = this.child[this.childProp];
     };
     updateChild = () => {
         console.log('updateChild');
+        this.child[this.childProp] = this.host[this.hostProp];
     };
 }
--- BoundInstance.ts
+++ BoundInstance.ts
@@ -2,16 +2,24 @@ import {BindingOptions} from './types';
 import {subscribe} from 'trans-render/lib/subscribe.js';
 
 export class BoundInstance{
-    constructor(public childProp: string, public hostProp: string, child: Element, public host: Element, public options: BindingOptions | undefined){
-        subscribe(child, childProp, this.updateHost);
+    constructor(public childProp: string, public hostProp: string, public child: Element, public host: Element, public options: BindingOptions | undefined){
+        
+        if(child.localName === 'input' && childProp === 'value'){
+            child.addEventListener('input', this.updateHost);
+        }else{
+            subscribe(child, childProp, this.updateHost);
+        }
+            
         subscribe(host, hostProp, this.updateChild);
     }
 
     updateHost = () => {
         console.log('updateHost');
+        (this.host  as any)[this.hostProp] = (this.child as any)[this.childProp];
     }
 
     updateChild = () => {
         console.log('updateChild');
+        (this.child as any)[this.childProp] = (this.host as any)[this.hostProp];
     }
 }
