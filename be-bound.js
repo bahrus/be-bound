f1cf16ff53a8a7165ec0eeecd912e89e36aed78a
Bruce B. Anderson <anderson.bruce.b@gmail.com>
2024-11-27 23:17:32 -0500

    0.0.33

--- be-bound.js
+++ be-bound.js
@@ -138,6 +138,7 @@ class BeBound extends BE {
         const { localAbsObj, localShareObj, remoteAbsObj, remoteShareObj} = binding;
         const localVal = await localAbsObj.getValue();
         const remoteVal = await remoteAbsObj.getValue();
+        const {breakTie} = await import('trans-render/lib/breakTie.js');
         const hs = breakTie(localVal, remoteVal);
         switch (hs) {
             case 'lhs':
@@ -172,42 +173,42 @@ class BeBound extends BE {
         };
     }
 }
-const typeRankings = [
-    'undefined',
-    'null',
-    'string',
-    'boolean',
-    'number',
-    'bigint',
-    'symbol',
-    'object',
-    'function'
-];
-function breakTie(lhs, rhs) {
-    if (lhs === rhs)
-        return 'eq';
-    const lhsType = lhs === null ? 'null' : typeof lhs;
-    const rhsType = rhs === null ? 'null' : typeof rhs;
-    const lhsTypeScore = typeRankings.indexOf(lhsType);
-    const rhsTypeScore = typeRankings.indexOf(rhsType);
-    if (lhsTypeScore > rhsTypeScore)
-        return 'lhs';
-    if (rhsTypeScore > lhsTypeScore)
-        return 'rhs';
-    switch (lhsType) {
-        case 'string':
-            if (lhs.length > rhs.length)
-                return 'lhs';
-            if (rhs.length > lhs.length)
-                return 'rhs';
-        default:
-            if (lhs > rhs)
-                return 'lhs';
-            if (rhs > lhs)
-                return 'rhs';
-    }
-    return 'eq';
-}
+// const typeRankings = [
+//     'undefined',
+//     'null',
+//     'string',
+//     'boolean',
+//     'number',
+//     'bigint',
+//     'symbol',
+//     'object',
+//     'function'
+// ];
+// function breakTie(lhs, rhs) {
+//     if (lhs === rhs)
+//         return 'eq';
+//     const lhsType = lhs === null ? 'null' : typeof lhs;
+//     const rhsType = rhs === null ? 'null' : typeof rhs;
+//     const lhsTypeScore = typeRankings.indexOf(lhsType);
+//     const rhsTypeScore = typeRankings.indexOf(rhsType);
+//     if (lhsTypeScore > rhsTypeScore)
+//         return 'lhs';
+//     if (rhsTypeScore > lhsTypeScore)
+//         return 'rhs';
+//     switch (lhsType) {
+//         case 'string':
+//             if (lhs.length > rhs.length)
+//                 return 'lhs';
+//             if (rhs.length > lhs.length)
+//                 return 'rhs';
+//         default:
+//             if (lhs > rhs)
+//                 return 'lhs';
+//             if (rhs > lhs)
+//                 return 'rhs';
+//     }
+//     return 'eq';
+// }
 
 await BeBound.bootUp();
 export { BeBound };
